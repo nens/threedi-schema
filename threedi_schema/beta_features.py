50faad9b871a8c250b6ca559800d17c5666ae093
elisallenens <eli.salle@nelen-schuurmans.nl>
2023-03-22 14:56:50 +0100

    add file for marking columns as beta features

---
+++ threedi_schema/beta_features.py
@@ -0,0 +1,5 @@
+"""
+Put columns in beta development in this list to prevent users from using them, in the SQLAlchemy format used in the schema.
+For instance, models.BoundaryCondition1D.boundary_type
+"""
+BETA_FEATURES = []
