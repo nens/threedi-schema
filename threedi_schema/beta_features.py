590e286fa07874474d812ff5856a46133cbf6fcf
elisallenens <eli.salle@nelen-schuurmans.nl>
2023-08-16 12:16:19 +0200

    fix incorrectly formatted beta features file

--- threedi_schema/beta_features.py
+++ threedi_schema/beta_features.py
@@ -4,7 +4,22 @@ from .domain import constants, models
 Put features in beta development in these lists to prevent users from using them, in the SQLAlchemy format used in the schema.
 """
 """This list contains beta columns, e.g. models.SomeTable.some_column"""
-BETA_COLUMNS = [
+BETA_COLUMNS = []
+"""
+This list contains dicts with lists of beta values for columns, where the dict has the format:
+{
+    "columns": [
+        models.SomeTable.some_column,
+        models.SomeTable.some_other_column,
+    ],
+    "values": [
+        constants.SomeConstantsClass.SOMEENUMVALUE,
+        "or just some random string",
+    ]
+}
+The modelchecker will go through each column and give an error when a beta value is used for its associated column.
+"""
+BETA_VALUES = [
     {
         "columns": [
             models.GlobalSetting.frict_type,
@@ -20,18 +35,3 @@ BETA_COLUMNS = [
         ],
     }
 ]
-"""
-This list contains dicts with lists of beta values for columns, where the dict has the format:
-{
-    "columns": [
-        models.SomeTable.some_column,
-        models.SomeTable.some_other_column,
-    ],
-    "values": [
-        constants.SomeConstantsClass.SOMEENUMVALUE,
-        "or just some random string",
-    ]
-}
-The modelchecker will go through each column and give an error when a beta value is used for its associated column.
-"""
-BETA_VALUES = []
