00e9b477c2bc0291ac8ffcc7f69dde6f64513733
elisallenens <eli.salle@nelen-schuurmans.nl>
2023-08-17 15:22:14 +0200

    fix linter

--- threedi_schema/tests/test_beta_features.py
+++ threedi_schema/tests/test_beta_features.py
@@ -1,12 +1,15 @@
-# this import is a test in itself; if these variables aren't defined, the modelchecker will fail
-from threedi_schema.beta_features import BETA_COLUMNS, BETA_VALUES
-# comparing directly with SQLAlchemy Column type doesn't work
+# comparing directly with SQLAlchemy Column type doesn't work so we use InstrumentedAttribute
 from sqlalchemy.orm.attributes import InstrumentedAttribute
+
+# this import is a test in itself; if the beta variables aren't defined, the modelchecker will fail
+from threedi_schema.beta_features import BETA_COLUMNS, BETA_VALUES
+
 
 def test_beta_columns_structure():
     assert isinstance(BETA_COLUMNS, list)
     for column in BETA_COLUMNS:
         assert isinstance(column, InstrumentedAttribute)
+
 
 def test_beta_values_structure():
     assert isinstance(BETA_VALUES, list)
