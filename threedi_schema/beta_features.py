4cba53a8a8e1d776be902c82726732d1ea2b9dad
elisallenens <eli.salle@nelen-schuurmans.nl>
2023-08-17 15:04:24 +0200

    put docstrigs under instead of above variables

--- threedi_schema/beta_features.py
+++ threedi_schema/beta_features.py
@@ -3,22 +3,10 @@ from .domain import constants, models
 """
 Put features in beta development in these lists to prevent users from using them, in the SQLAlchemy format used in the schema.
 """
-"""This list contains beta columns, e.g. models.SomeTable.some_column"""
+
 BETA_COLUMNS = []
-"""
-This list contains dicts with lists of beta values for columns, where the dict has the format:
-{
-    "columns": [
-        models.SomeTable.some_column,
-        models.SomeTable.some_other_column,
-    ],
-    "values": [
-        constants.SomeConstantsClass.SOMEENUMVALUE,
-        "or just some random string",
-    ]
-}
-The modelchecker will go through each column and give an error when a beta value is used for its associated column.
-"""
+"""This list contains beta columns, e.g. models.SomeTable.some_column"""
+
 BETA_VALUES = [
     {
         "columns": [
@@ -35,3 +23,17 @@ BETA_VALUES = [
         ],
     }
 ]
+"""
+This list contains dicts with lists of beta values for columns, where the dict has the format:
+{
+    "columns": [
+        models.SomeTable.some_column,
+        models.SomeTable.some_other_column,
+    ],
+    "values": [
+        constants.SomeConstantsClass.SOMEENUMVALUE,
+        "or just some random string",
+    ]
+}
+The modelchecker will go through each column and give an error when a beta value is used for its associated column.
+"""
