f90ee1b5a2e597ae492e80f2491e280fcd33ac67
Margriet Palm <margriet.palm@nelen-schuurmans.nl>
2024-01-23 10:16:49 +0100

    Fix migration

--- threedi_schema/migrations/versions/0219_nullable_cross_section_location_friction_value.py
+++ threedi_schema/migrations/versions/0219_nullable_cross_section_location_friction_value.py
@@ -1,8 +1,8 @@
 """rename vegetation columns
 
-Revision ID: 0218
-Revises: 0217
-Create Date: 2023-12-22 08:33
+Revision ID: 0219
+Revises: 0218
+Create Date: 2024-01-22 15:00
 
 """
 
@@ -15,7 +15,18 @@ down_revision = '0218'
 branch_labels = None
 depends_on = None
 
+MIGRATION_QUERIES = """
+SELECT RecoverGeometryColumn('v2_cross_section_location', 'the_geom', 4326, 'POINT', 'XY')
+"""
+
 
 def upgrade():
     with op.batch_alter_table("v2_cross_section_location") as batch_op:
-        batch_op.alter_column("friction_value", nullable=True, type_=sa.TEXT)
+        batch_op.alter_column("friction_value", nullable=True, type_=sa.Float)
+    for q in MIGRATION_QUERIES.split(";"):
+        op.execute(sa.text(q))
+
+
+def downgrade():
+    with op.batch_alter_table("v2_cross_section_location") as batch_op:
+        batch_op.alter_column("friction_value", nullable=False, type_=sa.Float)
