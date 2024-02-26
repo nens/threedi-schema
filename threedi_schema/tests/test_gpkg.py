d2daa5a841aaa8ced6db89e2f4df489cb1427a53
Margriet Palm <margriet.palm@nelen-schuurmans.nl>
2024-02-26 15:33:59 +0100

    Fix catching warnings

--- threedi_schema/tests/test_gpkg.py
+++ threedi_schema/tests/test_gpkg.py
@@ -8,7 +8,6 @@ def test_convert_to_geopackage(oldest_sqlite):
     # In case the fixture changes and refers to a geopackage,
     # convert_to_geopackage will be ignored because the db is already a geopackage
     assert oldest_sqlite.get_engine().dialect.name == "sqlite"
-    oldest_sqlite.schema.upgrade(convert_to_geopackage=True)
     # check if ogr2ogr is installed and has the right version:
     result = subprocess.run(
         "ogr2ogr --version",
@@ -26,12 +25,14 @@ def test_convert_to_geopackage(oldest_sqlite):
         if float_version >= 3.4:
             expect_success = True
     if expect_success:
+        oldest_sqlite.schema.upgrade(convert_to_geopackage=True)
         # Ensure that after the conversion the geopackage is used
         assert oldest_sqlite.path.suffix == ".gpkg"
         assert oldest_sqlite.get_engine().dialect.name == "geopackage"
         assert oldest_sqlite.schema.validate_schema()
     else:
         with pytest.warns():
+            oldest_sqlite.schema.upgrade(convert_to_geopackage=True)
             assert oldest_sqlite.path.suffix == ".sqlite"
             assert oldest_sqlite.get_engine().dialect.name == "sqlite"
             assert oldest_sqlite.schema.validate_schema()
