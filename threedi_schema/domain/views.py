8a692b8b7eced6882913ce85f510f0208f4dd728
elisalle <eli.salle@nelen-schuurmans.nl>
2024-08-21 12:17:17 +0200

    add missing comma

--- threedi_schema/domain/views.py
+++ threedi_schema/domain/views.py
@@ -3,7 +3,7 @@ VIEWS_TO_DELETE = [
     "v2_pipe_map_view",
     "v2_imp_surface_view",
     "v2_1d_lateral_view",
-    "v2_1d_boundary_conditions_view"
+    "v2_1d_boundary_conditions_view",
 ]
 ALL_VIEWS = {
     "lateral_1d_view": {
