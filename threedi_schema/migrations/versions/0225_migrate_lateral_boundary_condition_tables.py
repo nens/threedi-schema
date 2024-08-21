c275607c2ca60c9431db098ea030ca961637964a
elisalle <eli.salle@nelen-schuurmans.nl>
2024-08-21 10:56:12 +0200

    rename all columns without batch rename

--- threedi_schema/migrations/versions/0225_migrate_lateral_boundary_condition_tables.py
+++ threedi_schema/migrations/versions/0225_migrate_lateral_boundary_condition_tables.py
@@ -74,26 +74,22 @@ NEW_GEOM_COLUMNS = {
     ("boundary_condition_1d", Column("geom", Geometry("POINT"), nullable=False)),
 }
 
+
 # old name, new name
-# the columns will be individually renamed
+# the columns will be renamed using raw sql
 # this is because alembic has conniptions whenever you try to batch rename a geometry column
-RENAME_GEOM_COLUMNS = {
-    "lateral_2d": [
-        ("the_geom", "geom"),
-    ],
-    "boundary_condition_2d": [
-        ("the_geom", "geom"),
-    ]
-}
-
-
-# these columns can be batch renamed via alembic
 RENAME_COLUMNS = {
     "boundary_condition_1d": [
         ("boundary_type", "type"),
     ],
     "boundary_condition_2d": [
         ("boundary_type", "type"),
+    ],
+    "lateral_2d": [
+        ("the_geom", "geom"),
+    ],
+    "boundary_condition_2d": [
+        ("the_geom", "geom"),
     ],
 }
 
@@ -145,14 +141,6 @@ def add_columns_to_tables(table_columns: List[Tuple[str, Column]]):
                 batch_op.add_column(col)
 
 
-def rename_columns(table: str, columns: List[Tuple[str, str]]):
-    with op.batch_alter_table(table) as batch_op:
-        for column_set in columns:
-            batch_op.alter_column(
-                column_set[0], new_column_name=column_set[1]
-            )
-
-
 def add_geometry_column(table: str, geocol: Column):
     # Adding geometry columns via alembic doesn't work
     # https://postgis.net/docs/AddGeometryColumn.html
@@ -162,7 +150,7 @@ def add_geometry_column(table: str, geocol: Column):
     op.execute(sa.text(query))
 
 
-def rename_geom_columns(table_name: str, columns: List[Tuple[str, str]]):
+def rename_columns(table_name: str, columns: List[Tuple[str, str]]):
     # no checks for existence are done, this will fail if table or any source column doesn't exist
     connection = op.get_bind()
     old_columns_result = connection.execute(sa.text(f"PRAGMA table_info('{table_name}')")).fetchall()
@@ -234,8 +222,6 @@ def upgrade():
     # rename columns in renamed tables
     for table_name, columns in RENAME_COLUMNS.items():
         rename_columns(table_name, columns)
-    for table_name, columns in RENAME_GEOM_COLUMNS.items():
-        rename_geom_columns(table_name, columns)
 
     # recover geometry column data from connection nodes
     for table, column in (
