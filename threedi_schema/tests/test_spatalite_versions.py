c477d6625a14123c3b6724589e7918350da99e83
Margriet Palm <margriet.palm@nelen-schuurmans.nl>
2024-09-02 09:27:23 +0200

    Modify test_copy_invalid_geometry to be independent of the actual schema

--- threedi_schema/tests/test_spatalite_versions.py
+++ threedi_schema/tests/test_spatalite_versions.py
@@ -1,7 +1,8 @@
-import pytest
-from sqlalchemy import func
+from sqlalchemy import Column, func, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 from threedi_schema.domain import models
+from threedi_schema.domain.custom_types import Geometry
 from threedi_schema.infrastructure.spatialite_versions import (
     copy_model,
     get_spatialite_version,
@@ -42,31 +43,43 @@ def test_copy_model(empty_sqlite_v3, empty_sqlite_v4):
         assert records == [(3, "test", "POINT(-71.064544 42.28787)")]
 
 
-@pytest.mark.skip("Cannot pass due to changed model")
+# @pytest.mark.skip("Cannot pass due to changed model")
 def test_copy_invalid_geometry(empty_sqlite_v3, empty_sqlite_v4):
     """Copying an invalid geometry (ST_IsValid evaluates to False) is possible"""
     db_from = empty_sqlite_v3
     db_to = empty_sqlite_v4
-    # Note MP: this only works when this object is not involved in a migration
-    # This may cause issues with future database upgrades
-    obj = models.GridRefinementArea(
+    # Create GridRefinementArea on the fly to match database scheme in the used sqlitest
+    Base = declarative_base()
+    TestModel = type(
+        "TestModel",
+        (Base,),
+        {
+            "__tablename__": "v2_grid_refinement_area",
+            "id": Column(Integer, primary_key=True),
+            "display_name": Column(String(255)),
+            "code": Column(String(100)),
+            "the_geom": Column(
+                Geometry("POINT")
+            ),  # Use Text as a placeholder for Geometry
+        },
+    )
+    obj = TestModel(
         id=3,
         code="test",
         display_name="test",
-        geom="SRID=4326;POLYGON((0 0, 10 10, 0 10, 10 0, 0 0))",
-        grid_level=1,
+        the_geom="SRID=4326;POLYGON((0 0, 10 10, 0 10, 10 0, 0 0))",
     )
     with db_from.session_scope() as session:
         session.add(obj)
         session.commit()
 
-    copy_model(db_from, db_to, models.GridRefinementArea)
+    copy_model(db_from, db_to, TestModel)
 
     with db_to.session_scope() as session:
         records = list(
             session.query(
-                models.GridRefinementArea.id,
-                func.ST_AsText(models.GridRefinementArea.the_geom),
+                TestModel.id,
+                func.ST_AsText(TestModel.the_geom),
             )
         )
 
