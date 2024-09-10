1b4c9919a0bdb27692a902ac8ad06934a2330627
margrietpalm <margrietpalm@users.noreply.github.com>
2024-09-10 10:33:57 +0200

    schema 300 - 2d 1d2d (#83)
    * Implement schema changes and migration
    * Modify test_spatialite_versions to be independent of the actual schema

--- threedi_schema/tests/test_spatalite_versions.py
+++ threedi_schema/tests/test_spatalite_versions.py
@@ -1,6 +1,8 @@
-from sqlalchemy import func
+from sqlalchemy import Column, func, Integer, String
+from sqlalchemy.orm import declarative_base
 
 from threedi_schema.domain import models
+from threedi_schema.domain.custom_types import Geometry
 from threedi_schema.infrastructure.spatialite_versions import (
     copy_model,
     get_spatialite_version,
@@ -16,11 +18,22 @@ def test_get_spatialite_version(empty_sqlite_v3):
 def test_copy_model(empty_sqlite_v3, empty_sqlite_v4):
     db_from = empty_sqlite_v3
     db_to = empty_sqlite_v4
-
-    # Add a record to 'db_from'
-    obj = models.ConnectionNode(
-        id=3, code="test", the_geom="SRID=4326;POINT(-71.064544 42.287870)"
+    # Create v2_grid_refinement_area on the fly to match database scheme in the used sqlitest
+    Base = declarative_base()
+    TestModel = type(
+        "TestModel",
+        (Base,),
+        {
+            "__tablename__": "v2_connection_nodes",
+            "id": Column(Integer, primary_key=True),
+            "code": Column(String(100)),
+            "the_geom": Column(
+                Geometry("POINT")
+            ),  # Use Text as a placeholder for Geometry
+        },
     )
+    # Add a record to 'db_from'
+    obj = TestModel(id=3, code="test", the_geom="SRID=4326;POINT(-71.064544 42.287870)")
     with db_from.session_scope() as session:
         session.add(obj)
         session.commit()
@@ -45,26 +58,38 @@ def test_copy_invalid_geometry(empty_sqlite_v3, empty_sqlite_v4):
     """Copying an invalid geometry (ST_IsValid evaluates to False) is possible"""
     db_from = empty_sqlite_v3
     db_to = empty_sqlite_v4
-    # Note MP: this only works when this object is not involved in a migratin
-    # This may cause issues with future database upgrades
-    obj = models.GridRefinementArea(
+    # Create v2_grid_refinement_area on the fly to match database scheme in the used sqlitest
+    Base = declarative_base()
+    TestModel = type(
+        "TestModel",
+        (Base,),
+        {
+            "__tablename__": "v2_grid_refinement_area",
+            "id": Column(Integer, primary_key=True),
+            "display_name": Column(String(255)),
+            "code": Column(String(100)),
+            "the_geom": Column(
+                Geometry("POINT")
+            ),  # Use Text as a placeholder for Geometry
+        },
+    )
+    obj = TestModel(
         id=3,
         code="test",
         display_name="test",
         the_geom="SRID=4326;POLYGON((0 0, 10 10, 0 10, 10 0, 0 0))",
-        refinement_level=1,
     )
     with db_from.session_scope() as session:
         session.add(obj)
         session.commit()
 
-    copy_model(db_from, db_to, models.GridRefinementArea)
+    copy_model(db_from, db_to, TestModel)
 
     with db_to.session_scope() as session:
         records = list(
             session.query(
-                models.GridRefinementArea.id,
-                func.ST_AsText(models.GridRefinementArea.the_geom),
+                TestModel.id,
+                func.ST_AsText(TestModel.the_geom),
             )
         )
 
