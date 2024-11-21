e929462f180f3dbe0ef18ef8f4ef1b5f3b72789a
Margriet Palm <margriet.palm@nelen-schuurmans.nl>
2024-11-21 13:43:59 +0100

    Add empty migration

---
+++ threedi_schema/migrations/versions/0229_clean_up.py
@@ -0,0 +1,24 @@
+"""
+
+Revision ID: 022
+9Revises:
+Create Date: 2024-11-15 14:18
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "0229"
+down_revision = "0228"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
