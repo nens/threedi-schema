416b0f5a61de88f553a5ceeb69bb1a7cb154f5d0
Margriet Palm <margriet.palm@nelen-schuurmans.nl>
2024-11-21 08:27:59 +0100

    Remove columns referencing v2 in geometry_column

--- threedi_schema/migrations/versions/0229_clean_up.py
+++ threedi_schema/migrations/versions/0229_clean_up.py
@@ -19,26 +19,37 @@ depends_on = None
 
 def remove_tables(tables: List[str]):
     for table in tables:
-            op.drop_table(table)
-
+        op.drop_table(table)
 
 
 def find_tables_by_pattern(pattern: str) -> List[str]:
     connection = op.get_bind()
-    query = connection.execute(sa.text(f"select name from sqlite_master where type = 'table' and name like '{pattern}'"))
+    query = connection.execute(
+        sa.text(f"select name from sqlite_master where type = 'table' and name like '{pattern}'"))
     return [item[0] for item in query.fetchall()]
 
 
 def remove_old_tables():
     remaining_v2_idx_tables = find_tables_by_pattern('idx_v2_%_the_geom')
     remaining_alembic = find_tables_by_pattern('%_alembic_%_the_geom')
-    remove_tables(remaining_v2_idx_tables+remaining_alembic)
+    remove_tables(remaining_v2_idx_tables + remaining_alembic)
+
+
+def clean_geometry_columns():
+    """ Remove columns referencing v2 in geometry_columns """
+    op.execute(sa.text("""
+            DELETE FROM geometry_columns WHERE f_table_name IN (
+                SELECT g.f_table_name FROM geometry_columns g
+                LEFT JOIN sqlite_master m ON g.f_table_name = m.name
+                WHERE m.name IS NULL AND g.f_table_name like "%v2%"
+            );
+        """))
+
 
 def upgrade():
     remove_old_tables()
+    clean_geometry_columns()
 
 
 def downgrade():
     pass
-
-
