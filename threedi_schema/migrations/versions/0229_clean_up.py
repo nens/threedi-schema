287b6db85cfe66f2ebab9b00a82ce02b4d7539a5
margrietpalm <margrietpalm@users.noreply.github.com>
2024-11-26 14:17:40 +0100

    Remove left over references in geometry columns (#139)
    From here schema upgrades will require spatialite 5!!!!

--- threedi_schema/migrations/versions/0229_clean_up.py
+++ threedi_schema/migrations/versions/0229_clean_up.py
@@ -5,7 +5,6 @@ Revision ID: 022
 Create Date: 2024-11-15 14:18
 
 """
-
 from typing import List
 
 import sqlalchemy as sa
@@ -20,23 +19,46 @@ depends_on = None
 
 def remove_tables(tables: List[str]):
     for table in tables:
-            op.drop_table(table)
-
+        op.drop_table(table)
 
 
 def find_tables_by_pattern(pattern: str) -> List[str]:
     connection = op.get_bind()
-    query = connection.execute(sa.text(f"select name from sqlite_master where type = 'table' and name like '{pattern}'"))
+    query = connection.execute(
+        sa.text(f"select name from sqlite_master where type = 'table' and name like '{pattern}'"))
     return [item[0] for item in query.fetchall()]
 
 
 def remove_old_tables():
     remaining_v2_idx_tables = find_tables_by_pattern('idx_v2_%_the_geom')
     remaining_alembic = find_tables_by_pattern('%_alembic_%_the_geom')
-    remove_tables(remaining_v2_idx_tables+remaining_alembic)
+    remove_tables(remaining_v2_idx_tables + remaining_alembic)
+
+
+def clean_geometry_columns():
+    """ Remove columns referencing v2 in geometry_columns """
+    op.execute(sa.text("""
+            DELETE FROM geometry_columns WHERE f_table_name IN (
+                SELECT g.f_table_name FROM geometry_columns g
+                LEFT JOIN sqlite_master m ON g.f_table_name = m.name
+                WHERE m.name IS NULL AND g.f_table_name like "%v2%"
+            );
+        """))
+
+
+def clean_triggers():
+    """ Remove triggers referencing v2 tables """
+    connection = op.get_bind()
+    triggers = [item[0] for item in connection.execute(
+        sa.text("SELECT tbl_name FROM sqlite_master WHERE type='trigger' AND tbl_name LIKE '%v2%';")).fetchall()]
+    for trigger in triggers:
+        op.execute(f"DROP TRIGGER IF EXISTS {trigger};")
+
 
 def upgrade():
     remove_old_tables()
+    clean_geometry_columns()
+    clean_triggers()
 
 
 def downgrade():
