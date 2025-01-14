cdc3f0851d744a49c72570f9811c6ca40f494556
Margriet Palm <margriet.palm@nelen-schuurmans.nl>
2025-01-14 09:48:42 +0100

    bump version

--- threedi_schema/__init__.py
+++ threedi_schema/__init__.py
@@ -2,7 +2,7 @@ from .application import *  # NOQA
 from .domain import constants, custom_types, models  # NOQA
 
 # fmt: off
-__version__ = '0.230.0.dev1'
+__version__ = '0.230.0.dev2'
 
 
 # fmt: on
